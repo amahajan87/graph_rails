a57f4948de331a42ea1dd56c2650ea0509cd5b5c
Aakanksha Mahajan <aakankm@bgsu.edu>
2020-04-06 18:37:43 -0400

    Added status to payments and updated seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_200915) do
+ActiveRecord::Schema.define(version: 2020_04_06_223643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_200915) do
     t.float "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
 end
